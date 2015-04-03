8396219a4425a84d79303febffeae54b2347c0e4
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-03 17:50:39 -0400

    Nodes write to a common csv file.  Also, fixed threading behavior: use a thread pool.

---
+++ threadPool.py
@@ -0,0 +1,377 @@
+###############################################################################
+#   lazyflow: data flow based lazy parallel computation framework
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the Lesser GNU General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# See the files LICENSE.lgpl2 and LICENSE.lgpl3 for full text of the
+# GNU Lesser General Public License version 2.1 and 3 respectively.
+# This information is also available on the ilastik web site at:
+#		   http://ilastik.org/license/
+###############################################################################
+# Built-in
+import atexit
+import collections
+import heapq
+import threading
+import platform
+import time
+import os
+import ctypes
+
+import psutil
+
+
+# This module's code needs to be sanitized if you're not using CPython.
+# In particular, check that deque operations like push() and pop() are still atomic.
+assert platform.python_implementation() == "CPython"
+
+class PriorityQueue(object):
+    """
+    Simple threadsafe heap based on the python heapq module.
+    """
+    def __init__(self):
+        self._heap = []
+        self._lock = threading.Lock()
+
+    def push(self, item):
+        with self._lock:
+            heapq.heappush(self._heap, item)
+    
+    def pop(self):
+        with self._lock:
+            return heapq.heappop(self._heap)
+    
+    def __len__(self):
+        return len(self._heap)
+
+class FifoQueue(object):
+    """
+    Simple FIFO queue based on collections.deque.
+    """
+    def __init__(self):
+        self._deque = collections.deque() # Documentation says this is threadsafe for push and pop
+
+    def push(self, item):
+        self._deque.append(item)
+    
+    def pop(self):
+        return self._deque.popleft()
+    
+    def __len__(self):
+        return len(self._deque)
+
+class LifoQueue(object):
+    """
+    Simple LIFO queue based on collections.deque.
+    """
+    def __init__(self):
+        self._deque = collections.deque() # Documentation says this is threadsafe for push and pop
+
+    def push(self, item):
+        self._deque.append(item)
+    
+    def pop(self):
+        return self._deque.pop()
+    
+    def __len__(self):
+        return len(self._deque)
+
+
+class MemoryWatcher(threading.Thread):
+    """
+    Background thread for checking memory usage
+    """
+    
+    def __init__(self, thread_pool):
+        threading.Thread.__init__(self)
+        self.process = psutil.Process(os.getpid())
+        self.usage = self.process.memory_percent()
+        self.tasks = FifoQueue()
+        self.thread_pool = thread_pool
+        self.threshold = 85 # threshold at which to 
+        self.threshold_reached = False
+        self.stopped = False
+        self.daemon = True
+        
+    def run(self):
+        """
+        Computes memory usage every 0.1 second.
+        Flushes queued tasks, if memory usage is below a threshold.
+        """
+        while self.stopped is False:
+            self.usage = self.process.memory_percent()
+            if self.usage < self.threshold:
+                self.flush()
+            time.sleep(0.1)
+            
+    def filter(self, task):
+        """
+        See if a task needs to be queued due to high memory usages.
+        """
+        if self.usage > self.threshold and len(task._priority) == 1:
+            if self.threshold_reached is False:
+                print "MemoryWatcher: memory usage above %f%% filtering task." % (self.threshold,)
+                self.threshold_reached = True
+            self.tasks.push(task)
+            raise IndexError
+        return task
+            
+    def flush(self):
+        """
+        Flush all queued tasks back to the threadpool.
+        """
+        if len(self.tasks) > 0:
+            if self.threshold_reached is True:
+                print "MemoryWatcher: memory usage below %f%%. Flushing queued tasks ..." % (self.threshold,)
+                self.threshold_reached = False
+                
+        while len(self.tasks) > 0:
+            task = self.tasks.pop()
+            self.thread_pool.wake_up(task)
+            
+    def stop(self):
+        """
+        stop the MemoryWatcher
+        """
+        self.stopped = True
+   
+class ThreadPool(object):
+    """
+    Manages a set of worker threads and dispatches tasks to them.
+    """
+
+    #_DefaultQueueType = FifoQueue
+    #_DefaultQueueType = LifoQueue
+    _DefaultQueueType = PriorityQueue
+    
+    def __init__(self, num_workers, queue_type=_DefaultQueueType):
+        """
+        Constructor.  Starts all workers.
+        
+        :param num_workers: The number of worker threads to create.
+        :param queue_type: The type of queue to use for prioritizing tasks.  Possible queue types include :py:class:`PriorityQueue`,
+                           :py:class:`FifoQueue`, and :py:class:`LifoQueue`, or any class with ``push()``, ``pop()``, and ``__len__()`` methods.
+        """
+        self.job_condition = threading.Condition()
+        self.unassigned_tasks = queue_type()
+        #self.memory = MemoryWatcher(self)
+        #self.memory.start()
+        self.num_workers = num_workers
+        self.workers = self._start_workers( num_workers, queue_type )
+
+        # ThreadPools automatically stop upon program exit
+        atexit.register( self.stop )
+
+    def wake_up(self, task):
+        """
+        Schedule the given task on the worker that is assigned to it.
+        If it has no assigned worker yet, assign it to the first worker that becomes available.
+        """
+        # Once a task has been assigned, it must always be processed in the same worker
+        if hasattr(task, 'assigned_worker') and task.assigned_worker is not None:
+            task.assigned_worker.wake_up( task )
+        else:
+            self.unassigned_tasks.push(task)
+            # Notify all currently waiting workers that there's new work
+            self._notify_all_workers()
+
+    def stop(self):
+        """
+        Stop all threads in the pool, and block for them to complete.
+        Postcondition: All worker threads have stopped.  Unfinished tasks are simply dropped.
+        """
+        #self.memory.stop()
+        
+        for w in self.workers:
+            w.stop()
+        
+        for w in self.workers:
+            w.join()
+    
+    def get_states(self):
+        return [w.state for w in self.workers]
+    
+    def _start_workers(self, num_workers, queue_type):
+        """
+        Start a set of workers and return the set.
+        """
+        workers = set()
+        for i in range(num_workers):
+            w = _Worker(self, i, queue_type=queue_type)
+            workers.add( w )
+            w.start()
+        return workers
+
+    def _notify_all_workers(self):
+        """
+        Wake up all worker threads that are currently waiting for work.
+        """
+        for worker in self.workers:
+            with worker.job_queue_condition:
+                worker.job_queue_condition.notify()
+
+    def _wait_for_idle(self):
+        """
+        Useful for testing only.
+        Wait until there are no tasks left in the threadpool.
+        """
+        done = False
+        while not done:
+            while self.unassigned_tasks:
+                time.sleep(0.1)
+            
+            for worker in self.workers:
+                while worker.job_queue:
+                    time.sleep(0.1)
+            
+            # Second pass: did any of those completing tasks launch new tasks?
+            done = True
+            for worker in self.workers:
+                if len(worker.job_queue) > 0:
+                    done = False
+            if self.unassigned_tasks:
+                done = False
+                    
+
+class _Worker(threading.Thread):
+    """
+    Runs in a loop until stopped.
+    The loop pops one task from the threadpool and executes it.
+    """
+
+    def __init__(self, thread_pool, index, queue_type ):
+        name = "Worker #{}".format(index)
+        super(_Worker, self).__init__( name=name )
+        self.daemon = True # kill automatically on application exit!
+        self.thread_pool = thread_pool
+        self.stopped = False
+        self.job_queue_condition = threading.Condition()
+        self.job_queue = queue_type()
+        self.state = 'initialized'
+        
+    def run(self):
+        """
+        Keep executing available tasks until we're stopped.
+        """
+        # Try to get some work.
+        self.state = 'waiting'
+        next_task = self._get_next_job()
+
+        while not self.stopped:
+            # Start (or resume) the work by switching to its greenlet
+            self.state = 'running task'
+            next_task()
+
+            # We're done with this request.
+            # Free it immediately for garbage collection.
+            self.state = 'freeing task'
+            next_task = None
+
+            # Now try to get some work (wait if necessary).
+            self.state = 'waiting'
+            next_task = self._get_next_job()
+
+    def stop(self):
+        """
+        Tell this worker to stop running.
+        Does not block for thread completion.
+        """
+        self.stopped = True
+        # Wake up the thread if it's waiting for work
+        with self.job_queue_condition:
+            self.job_queue_condition.notify()
+
+    def wake_up(self, task):
+        """
+        Add this task to the queue of tasks that are ready to be processed.
+        The task may or not be started already.
+        """
+        assert task.assigned_worker is self
+        with self.job_queue_condition:
+            self.job_queue.push(task)
+            self.job_queue_condition.notify()
+
+    def _get_next_job(self):
+        """
+        Get the next available job to perform.
+        If necessary, block until:
+            - a task is available (return it) OR
+            - the worker has been stopped (might return None)
+        """
+        # Keep trying until we get a job        
+        with self.job_queue_condition:
+            next_task = self._pop_job()
+
+            while next_task is None and not self.stopped:
+                # Wait for work to become available
+                self.job_queue_condition.wait()
+                next_task = self._pop_job()
+
+        if not self.stopped:
+            assert next_task is not None
+            assert next_task.assigned_worker is self
+
+        return next_task
+    
+    def _pop_job(self):
+        """
+        Non-blocking.
+        If possible, get a job from our own job queue.
+        Otherwise, get one from the global job queue.
+        Return None if neither queue has work to do.
+        """
+        # Try our own queue first
+        if len(self.job_queue) > 0:
+            return self.job_queue.pop()
+
+        # Otherwise, try to claim a job from the global unassigned list            
+        try:
+            #task = self.thread_pool.memory.filter(self.thread_pool.unassigned_tasks.pop())
+            task = self.thread_pool.unassigned_tasks.pop()
+        except IndexError:
+            return None
+        else:
+            task.assigned_worker = self # If this fails, then your callable is some built-in that doesn't allow arbitrary  
+                                        #  members (e.g. .assigned_worker) to be "monkey-patched" onto it.  You may have to wrap it in a custom class first.
+            return task
+    
+    def raise_exc(self, excobj):
+        """
+        I HAVEN'T TESTED THIS YET.  (But it looks useful.)
+        http://code.activestate.com/recipes/496960-thread2-killable-threads
+
+        Debugging method.
+        Asynchronously raise an exception in this thread.
+        See docstring for _async_raise() for more details.
+        """
+        assert self.isAlive(), "thread must be started"
+        for tid, tobj in threading._active.items():
+            if tobj is self:
+                _Worker._async_raise(tid, excobj)
+                return
+
+    @staticmethod
+    def _async_raise(tid, excobj):
+        """
+        Raise an exception in the thread with the given id.
+        http://code.activestate.com/recipes/496960-thread2-killable-threads
+        """
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(excobj))
+        if res == 0:
+            raise ValueError("nonexistent thread id")
+        elif res > 1:
+            # """if it returns a number greater than one, you're in trouble, 
+            # and you should call it again with exc=NULL to revert the effect"""
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
--- usecase_benchmarks.py
+++ usecase_benchmarks.py
@@ -5,14 +5,22 @@ import argparse
 import logging
 import threading
 import contextlib
+import collections
 from StringIO import StringIO
 from functools import partial
 
+import numpy as np
+import pandas as pd
+
 from elapsed_time import elapsed_time
 from dvid_url_fields import DvidUrlFields
+from fileLock import FileLock
 
-# roi_utils was copied from lazyflow/roi.py
+# roi_utils was copied from lazyflow/roi.p
 from roi_utils import getIntersectingRois
+
+# threadPool was copied from lazyflow/request/threadPool.py
+from threadPool import ThreadPool, LifoQueue
 
 log_formatter = logging.Formatter('%(levelname)s %(name)s %(message)s')
 log_handler = logging.StreamHandler(sys.stdout)
@@ -21,6 +29,13 @@ log_handler.setFormatter(log_formatter)
 CLUSTER_ACCESS_SERVER = 'login2'
 CLUSTER_PYTHON_EXE = '/groups/flyem/proj/cluster/miniconda/envs/stuart/bin/python'
 CLUSTER_NODE_TASK_SCRIPT = '/groups/flyem/proj/cluster/pydvid-benchmarks/usecase_benchmarks.py'
+
+CSV_COLUMNS = collections.OrderedDict( [ ('node-roi', 'str'),
+                                         ('node-roi-size', 'f4'),
+                                         ('num-requests', 'u4'),
+                                         ('num-threads', 'u4'),
+                                         ('elapsed-seconds', 'f4'),
+                                          ] )
 
 def main():
     # First arg tells us whether or not this is the master process.
@@ -62,18 +77,49 @@ def master_main():
              == len(parsed_args.node_blockshape) 
              == len(parsed_args.request_blockshape) )
 
+    if not os.path.exists( parsed_args.cwd ):
+        os.makedirs( parsed_args.cwd )
+    os.chdir( parsed_args.cwd )
+
+    # Remove old output files first.
+    with fab.cd(parsed_args.cwd):
+        rm_cmd = "rm -f *.output"
+        logger.info(rm_cmd)
+        fab.run( rm_cmd )
+
+    node_rois = getIntersectingRois( parsed_args.total_roi[1],
+                                     parsed_args.node_blockshape,
+                                     parsed_args.total_roi )
+
+
+    total_roi = np.array( parsed_args.total_roi )
+    total_volume = np.prod( total_roi[1] - total_roi[0] )
+    
+    num_request_rois = np.prod( parsed_args.node_blockshape ) / np.prod( parsed_args.request_blockshape )
+    
+    # Create an empty csv file.
+    csv_path = parsed_args.dvid_volume_url.hostname.replace(':', 'p')
+    csv_path += '-{:.1g}-px-{}-nodes-{}-cpus-{}-blocks'\
+                .format( total_volume, len(node_rois), parsed_args.threads_per_node, num_request_rois )
+    csv_path += '.csv'
+    init_csv(csv_path)
+
     def launch_node( node_roi, task_name ):
-        task_args = "node '{}' {} '{}' '{}' {}"\
+        task_args = "node '{}' {} '{}' '{}' {} {}"\
                     .format( task_name, 
                              parsed_args.dvid_volume_url,
                              node_roi, 
                              parsed_args.request_blockshape, 
-                             parsed_args.threads_per_node )
-
-        qsub_cmd = 'qsub -pe batch {num_cpus} -l short=true -N {task_name} -o {task_output_file} -j y -b y -cwd -V "{python_exe} {node_task_script} {task_args}"'\
+                             parsed_args.threads_per_node,
+                             csv_path )
+        
+        # If we choose our own output file name, then NFS can get confused if we delete and re-use the same name in quick succession.
+        # Let' just avoid the problem altogether by leaving the -o option out.
+        # -o {task_output_file}
+        qsub_cmd = 'qsub -pe batch {num_cpus} -l short=true -N {task_name} -j y -b y -cwd -V "{python_exe} {node_task_script} {task_args}"'\
                    .format( num_cpus=parsed_args.threads_per_node,
                             task_name=task_name,
-                            task_output_file=task_name + '.output',
+                            #task_output_file=task_name + '.output',
                             python_exe=CLUSTER_PYTHON_EXE,
                             node_task_script=CLUSTER_NODE_TASK_SCRIPT,
                             task_args=task_args )
@@ -89,15 +135,6 @@ def master_main():
             job_id_string = cmd_result.split()[2]
             return job_id_string
 
-    # Remove old output files first.
-    with fab.cd(parsed_args.cwd):
-        rm_cmd = "rm -f *.output"
-        logger.info(rm_cmd)
-        fab.run( rm_cmd )
-    
-    node_rois = getIntersectingRois( parsed_args.total_roi[1],
-                                     parsed_args.node_blockshape,
-                                     parsed_args.total_roi )
     job_ids = []
     for index, (start, stop) in enumerate(node_rois):
         job_id_str = launch_node( (list(start), list(stop)), "J{:04}".format(index) )
@@ -107,6 +144,11 @@ def master_main():
     wait_for_jobs(job_ids)
 
     logger.info( "FINISHED." )
+
+def init_csv(csv_path):
+    df = get_empty_dataframe()
+    with FileLock(csv_path):
+        df.to_csv(csv_path, header=True, mode='w', index=False)
 
 def wait_for_jobs( job_ids, hide_output=True, poll_interval=2.0 ):
     import fabric.api as fab
@@ -143,6 +185,7 @@ def node_main():
     parser.add_argument("node_roi", type=eval)
     parser.add_argument("request_blockshape", type=eval)
     parser.add_argument("num_threads", type=int)
+    parser.add_argument("csv_path", type=str)
     parsed_args = parser.parse_args()
 
     logger = logging.getLogger("node." + parsed_args.task_name)
@@ -163,20 +206,39 @@ def node_main():
         def request_block( block_roi ):
             node_data = dvid_accessor.get_ndarray( *block_roi )
 
-        threads = []
+        thread_pool = ThreadPool(parsed_args.num_threads, LifoQueue)
         for block_roi in block_rois:
-            t = threading.Thread(target=partial(request_block, block_roi))
-            threads.append( t )
-        
-        for t in threads:
-            t.start()
-        
+            thread_pool.wake_up( partial(request_block, block_roi) )
+
         with elapsed_time(logger) as elapsed:
-            for t in threads:
-                t.join()
+            thread_pool._wait_for_idle()
+
+        thread_pool.stop()
+
+        node_roi = np.array(parsed_args.node_roi)
+        node_roi_size = float(np.prod(node_roi[1] - node_roi[0]))
+
+        node_data = { 'node-roi'        : str(parsed_args.node_roi),
+                      'node-roi-size'   : node_roi_size,
+                      'elapsed-seconds' : elapsed.seconds,
+                      'num-threads'     : parsed_args.num_threads,
+                      'num-requests'    : len(block_rois) }
+
+        df = get_empty_dataframe().append( node_data, ignore_index=True )
+        with FileLock(parsed_args.csv_path):
+            df.to_csv(parsed_args.csv_path, header=False, mode='a', index=False)
 
     logger.info("DONE.")
-    
+
+def get_empty_dataframe():
+    """
+    Return an empty dataframe for nodes to use as a template for storing CSV data.
+    """
+    # This is the only way to initialize an empty DataFrame with a heterogeneous dtype.
+    empty_table = np.ndarray((0,), dtype=','.join(CSV_COLUMNS.values()))
+    df = pd.DataFrame(columns=CSV_COLUMNS.keys(), data=empty_table)
+    return df
+
 if __name__ == "__main__":
     # DEBUG ARGS
     if len(sys.argv) == 1:
@@ -184,11 +246,11 @@ if __name__ == "__main__":
         if DEBUG_MODE == "master":
             sys.argv += [ "master",
                           "http://emdata1:8000/api/repo/1c6ebbd7870511e4b3dc90b11c576b54/grayscale",
-                          "[(0,100,1000,1000), (1,200,2000,2000)]",
-                          "(1,10,1000,1000)",
-                          "(1,10,250,250)",
-                          "4",
-                          "--cwd=/groups/flyem/home/bergs/dvid_testing" ]
+                          "[(0,1000,1000,1000), (1,3000,3000,2000)]",
+                          "(1,500,500,500)",
+                          "(1,250,250,250)",
+                          "8",
+                          "--cwd=/groups/flyem/proj/cluster/dvid_testing" ]
             
         elif DEBUG_MODE == 'node':
             sys.argv.append("node")
